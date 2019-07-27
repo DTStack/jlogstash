84a164f25993933d4146e5f3adb8e9d679ce0aae
953372946@qq.com <953372946@qq.com>
2019-07-27 09:29:33 +0800

    use textoutputformat

--- pipeline/outputs/hive/src/main/java/com/dtstack/jlogstash/format/dirty/DirtyDataManager.java
+++ pipeline/outputs/hive/src/main/java/com/dtstack/jlogstash/format/dirty/DirtyDataManager.java
@@ -22,58 +22,69 @@ import com.alibaba.fastjson.JSONObject;
 import com.dtstack.jlogstash.format.TableInfo;
 import com.dtstack.jlogstash.format.util.DateUtil;
 import com.dtstack.jlogstash.format.util.HostUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextOutputFormatBak;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Date;
 import java.util.Map;
-import java.util.UUID;
 
 public class DirtyDataManager {
 
+    private static Logger logger = LoggerFactory.getLogger(DirtyDataManager.class);
+
     private TableInfo tableInfo;
     private Configuration config;
-    private BufferedWriter bw;
+    private JobConf jobConf;
+    private FileOutputFormat outputFormat;
+    private RecordWriter recordWriter;
+
 
     public DirtyDataManager(TableInfo tableInfo, Configuration configuration) {
         this.tableInfo = tableInfo;
         this.config = configuration;
+        this.jobConf = new JobConf(config);
+        outputFormat = new TextOutputFormatBak<>();
     }
 
     public void writeData(Map row, Throwable ex) {
-        String content = JSONObject.toJSONString(row);
-        String line = StringUtils.join(new String[]{content, JSONObject.toJSONString(ex.toString()), DateUtil.timestampToString(new Date())}, tableInfo.getDelimiter());
         try {
-            bw.write(line);
+            String content = JSONObject.toJSONString(row);
+            recordWriter.write(NullWritable.get(), new Text(org.apache.hadoop.util.StringUtils.join(tableInfo.getDelimiter(), new String[]{content, JSONObject.toJSONString(ex.toString()), DateUtil.timestampToString(new Date())})));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("{}", e);
         }
     }
 
     public void open() {
         try {
-            FileSystem fs = FileSystem.get(config);
             String location = String.format("%s/%s-%d.txt", tableInfo.getPath(), HostUtil.getHostName(), Thread.currentThread().getId());
-            Path path = new Path(location);
-            bw = new BufferedWriter(new OutputStreamWriter(fs.create(path, false)));
+            FileOutputFormat.setOutputPath(jobConf, new Path(location));
+            this.recordWriter = this.outputFormat.getRecordWriter(null, jobConf, location, Reporter.NULL);
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void close() {
-        if (bw != null) {
-            try {
-                bw.flush();
-                bw.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try {
+            RecordWriter<?, ?> rw = this.recordWriter;
+            if (rw != null) {
+                rw.close(Reporter.NULL);
             }
+        } catch (IOException e) {
+            logger.error("{}", e);
         }
     }
 
